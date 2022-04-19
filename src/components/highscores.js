6449a4bc2cbe5b91d0878ad54b2e2a5233e06b7a
Luca Silvestri <luca.silvestri@amanox.ch>
2022-04-19 15:25:44 +0200

    Fixed not running in Chrome. Fixed lifecycle issues with high score board.

--- src/components/highscores.js
+++ src/components/highscores.js
@@ -1,105 +1,107 @@
 /* globals AFRAME ABLAST THREE */
-AFRAME.registerSystem('highscores', {
-  schema: {
-    maxScores: {default: 10},
-  },
+  AFRAME.registerSystem('highscores', {
+    schema: {
+      maxScores: { default: 10 },
+    },
 
-  init: function () {
-    if (!this.isHighScore()) {
-      console.warn('Highscore can\'t be loaded or saved as no localStorage support found!');
-      return;
-    }
-
-    this.scores = [];
-    if (localStorage['highscores']) {
-      this.scores = JSON.parse(localStorage['highscores']);
-    } else {
-      for (var i = 0; i < 5; i++) {
-        this.scores[i] = {
-          name: 'nobody',
-          points: i*10,
-          shoots: 0,
-          time: 0,
-          validShoot: 0
-        };
+    init: function () {
+      if (!this.isHighScore()) {
+        console.warn('Highscore can\'t be loaded or saved as no localStorage support found!');
+        return;
       }
-    }
 
-    var self = this;
-    var ablastUI = document.getElementById('ablast-ui');
-    document.getElementById('save-score').addEventListener('click', function (event) {
-      ABLAST.currentScore.name = document.getElementById('player-name').value;
-      self.addNewScore(ABLAST.currentScore);
-      ablastUI.style.display = 'none';
-    });
-
-    this.sceneEl.addEventListener('gamestate-changed', function (evt) {
-      if ('state' in evt.detail.diff) {
-        if (evt.detail.state.state === 'STATE_GAME_OVER' || evt.detail.state.state === 'STATE_GAME_WIN') {
-          ablastUI.style.display = self.isHighScore(ABLAST.currentScore) ? 'block' : 'none';
+      this.scores = [];
+      if (localStorage['highscores']) {
+        this.scores = JSON.parse(localStorage['highscores']);
+      } else {
+        for (var i = 0; i < 5; i++) {
+          this.scores[i] = {
+            name: 'nobody',
+            points: i * 10,
+            shoots: 0,
+            time: 0,
+            validShoot: 0
+          };
         }
       }
-    });
-  },
 
-  isHighScore: function ()
-  {
-    try {
-    	return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
+      var self = this;
+
+      window.onload = function () {
+        var ablastUI = document.getElementById('ablast-ui');
+        document.getElementById('save-score').addEventListener('click', function (event) {
+          ABLAST.currentScore.name = document.getElementById('player-name').value;
+          self.addNewScore(ABLAST.currentScore);
+          ablastUI.style.display = 'none';
+        });
+
+        self.sceneEl.addEventListener('gamestate-changed', function (evt) {
+          if ('state' in evt.detail.diff) {
+            if (evt.detail.state.state === 'STATE_GAME_OVER' || evt.detail.state.state === 'STATE_GAME_WIN') {
+              ablastUI.style.display = self.isHighScore(ABLAST.currentScore) ? 'block' : 'none';
+            }
+          }
+        });
+      };
+    },
+
+    isHighScore: function () {
+      try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+      } catch (e) {
+        return false;
+      }
+    },
+
+    shouldStoreScore: function (data) {
+      return (this.scores.length < this.data.maxScores ||
+        this.scores[this.scores.length - 1].points < data.points);
+    },
+
+    addNewScore: function (data) {
+      // Check if we need to insert it
+      if (this.shouldStoreScore(data)) {
+        this.scores.push(data);
+
+        this.scores.sort(function (a, b) {
+          return parseInt(a.points) <= parseInt(b.points);
+        });
+
+        if (this.scores.length > this.data.maxScores) {
+          this.scores.pop();
+        }
+
+        localStorage['highscores'] = JSON.stringify(this.scores);
+        this.sceneEl.emit('highscores-updated', this.scores);
+        return true;
+      }
       return false;
     }
-  },
+  });
 
-  shouldStoreScore: function (data) {
-    return (this.scores.length < this.data.maxScores ||
-      this.scores[this.scores.length - 1].points < data.points);
-  },
+  AFRAME.registerComponent('highscores', {
+    schema: {},
 
-  addNewScore: function (data) {
-    // Check if we need to insert it
-    if (this.shouldStoreScore(data)) {
-      this.scores.push(data);
+    init: function () {
+      var el = this.el;
+      var self = this;
+      var sceneEl = this.el.sceneEl;
 
-      this.scores.sort(function(a,b) {
-        return parseInt(a.points) <= parseInt(b.points);
+      sceneEl.addEventListener('highscores-updated', function (event) {
+        el.setAttribute('bmfont-text', { text: buildText(self.system.scores) });
       });
 
-      if (this.scores.length > this.data.maxScores) {
-        this.scores.pop();
-      }
-
-      localStorage['highscores'] = JSON.stringify(this.scores);
-      this.sceneEl.emit('highscores-updated', this.scores);
-      return true;
+      el.setAttribute('bmfont-text', {
+        fntImage: 'assets/fonts/mozillavr.png',
+        fnt: 'assets/fonts/mozillavr.fnt',
+        scale: 0.0015,
+        baseline: 'top',
+        lineHeight: 90,
+        text: buildText(this.system.scores),
+        color: '#24caff'
+      });
     }
-    return false;
-  }
-});
-
-AFRAME.registerComponent('highscores', {
-  schema: {},
-
-  init: function () {
-    var el = this.el;
-    var self = this;
-    var sceneEl = this.el.sceneEl;
-
-    sceneEl.addEventListener('highscores-updated', function (event) {
-      el.setAttribute('bmfont-text', {text: buildText(self.system.scores)});
-    });
-
-    el.setAttribute('bmfont-text', {
-      fntImage: 'assets/fonts/mozillavr.png',
-      fnt: 'assets/fonts/mozillavr.fnt',
-      scale: 0.0015,
-      baseline: 'top',
-      lineHeight: 90,
-      text: buildText(this.system.scores),
-      color: '#24caff'
-    });
-  }
-});
+  });
 
 function buildText (scores) {
   var text = '';
